e8516120418c72c4ed534c06f8e7fc6d3dcd2c52
Active <56242467+doubleactii@users.noreply.github.com>
2022-05-27 17:42:47 -0500

    fix a memory leak
    An element of a collected array was removed and then referenced via it's index to delete it. This made it so the element was never actually deleted.

--- plugin/aRecycle.js
+++ plugin/aRecycle.js
@@ -18,7 +18,7 @@
 
 		VS.Client.___EVITCA_aRecycle = true;
 		VS.Client.aRecycle = aRecycle;
-		VS.World.global.aRecycle = aRecycle;
+		VS.global.aRecycle = aRecycle;
 
 		aRecycle.setMaxLimit = function(pLimit) {
 			if (typeof(pLimit) === 'number') {
@@ -54,12 +54,13 @@
 						// if you try to collect a diob to be recycled and the collection you are recyling it to is full, it is deleted instead.
 						if (pCollection.length >= COLLECTION_LIMIT) {
 							for (let i = pCollected.length - 1; i >= 0; i--) {
-								if (pCollected[i].constructor === Diob) {
-									this.cleanDiob(pCollected[i]);
-									VS.delDiob(pCollected[i]);
-								} else if (Object.keys(pCollected[i]).length) {
-									this.cleanDiob(pCollected[i]);
-									VS.delObject(pCollected[i]);
+								const diob = pCollected[i];
+								if (diob.constructor === Diob) {
+									this.cleanDiob(diob);
+									VS.delDiob(diob);
+								} else if (Object.keys(diob).length) {
+									this.cleanDiob(diob);
+									VS.delObject(diob);
 								}
 							}
 							return;
@@ -67,23 +68,25 @@
 						// if this recycledArray has more diobs than the collection can handle, the access is deleted
 						if (pCollected.length + pCollection.length > COLLECTION_LIMIT) {
 							const remainder = pCollected.length - (COLLECTION_LIMIT - pCollection.length);
-							for (let c = pCollected.length; c >= remainder; c--) {
-								if (pCollected[c].constructor === Diob) {
+							for (let c = pCollected.length - 1; c >= remainder; c--) {
+								const diob = pCollected[c];
+								if (diob.constructor === Diob) {
 									pCollected.splice(c, 1);
-									this.cleanDiob(pCollected[c]);
-									VS.delDiob(pCollected[c]);
-								} else if (Object.keys(pCollected).length) {
+									this.cleanDiob(diob);
+									VS.delDiob(diob);
+								} else if (Object.keys(diob).length) {
 									pCollected.splice(c, 1);
-									this.cleanDiob(pCollected[c]);
-									VS.delObject(pCollected[c]);
+									this.cleanDiob(diob);
+									VS.delObject(diob);
 								}
 							}
 						}
 						// the remaining diobs that are in the recycledArray is now cleaned and processed and added to the collection
 						for (let k = pCollected.length - 1; k >= 0; k--) {
-							if (pCollected[k].onCollected && typeof(pCollected[k].onCollected) === 'function') pCollected[k].onCollected();
-							if (!pCollection.includes(pCollected[k])) pCollection.push(pCollected[k]);
-							this.cleanDiob(pCollected[k]);
+							const diob = pCollected[k];
+							if (diob.onCollected && typeof(diob.onCollected) === 'function') diob.onCollected();
+							if (!pCollection.includes(diob)) pCollection.push(diob);
+							this.cleanDiob(diob);
 						}
 						return;
 					} else {
@@ -162,45 +165,48 @@
 		}
 
 		aRecycle.cleanDiob = function(pDiob) {
-			if (pDiob.constructor === Diob) {
-				if (pDiob.color) {
-					if (typeof(pDiob.color) === 'object' && pDiob.color.constructor === Object) {
-						pDiob.color.tint = 0xFFFFFF;
-						pDiob.color = pDiob.color;
-					} else {
-						pDiob.color = null;
+			if (pDiob) {
+				if (pDiob.constructor === Diob) {
+					const isInterface = (pDiob.baseType === 'Interface' || pDiob.type === 'Interface' || VS.Type.getInheritances(pDiob.type).includes('Interface'));
+					if (pDiob.color) {
+						if (typeof(pDiob.color) === 'object' && pDiob.color.constructor === Object) {
+							pDiob.color.tint = 0xFFFFFF;
+							pDiob.color = pDiob.color;
+						} else {
+							pDiob.color = null;
+						}
 					}
-				}
-				if (typeof(pDiob.scale) === 'object') {
-					pDiob.scale.x = pDiob.scale.y = 1;
-					pDiob.scale = pDiob.scale;
-				} else {
-					pDiob.scale = 1;
-				}
-				if (pDiob.baseType === 'Interface') {
-					if (pDiob.getInterfaceName()) {
+					if (typeof(pDiob.scale) === 'object') {
+						pDiob.scale.x = pDiob.scale.y = 1;
+						pDiob.scale = pDiob.scale;
+					} else {
+						pDiob.scale = 1;
+					}
+					if (isInterface) {
 						pDiob.hide();
 						VS.Client.removeInterfaceElement(pDiob.getInterfaceName(), pDiob, true);
 					}
+					pDiob.playAnimation();
+					pDiob.setTransition();
+					pDiob.angle = 0;
+					pDiob.alpha = 1;
+					pDiob.xPos = 0;
+					pDiob.yPos = 0;
+					if (!isInterface) {
+						pDiob.mapName = null;
+						// PINGABLE
+						pDiob.setLoc();
+					}
+					pDiob.text = '';
+					pDiob.composite = '';
+					if (pDiob.baseType === 'Movable') pDiob.move();
+					for (const o of pDiob.getOverlays()) pDiob.removeOverlay(o.type);
+					for (const fN of pDiob.getFilters()) pDiob.removeFilter(fN);
 				}
-				pDiob.playAnimation();
-				pDiob.setTransition();
-				pDiob.angle = 0;
-				pDiob.alpha = 1;
-				pDiob.xPos = null;
-				pDiob.yPos = null;
-				if (pDiob.baseType !== 'Interface') pDiob.mapName = null;
-				// PINGABLE
-				pDiob.setLoc();
-				pDiob.text = '';
-				pDiob.composite = '';
-				if (pDiob.baseType === 'Movable') pDiob.move();
-				for (const o of pDiob.getOverlays()) pDiob.removeOverlay(o.type);
-				for (const fN of pDiob.getFilters()) pDiob.removeFilter(fN);
+				pDiob.aRecycleCollected = true;
+				pDiob.inTicker = null;
+				if (pDiob.clean && typeof(pDiob.clean) === 'function') pDiob.clean();
 			}
-			pDiob.aRecycleCollected = true;
-			pDiob.inTicker = null;
-			if (pDiob.clean && typeof(pDiob.clean) === 'function') pDiob.clean();
 		}
 
 		aRecycle.toggleDebug = function() {
