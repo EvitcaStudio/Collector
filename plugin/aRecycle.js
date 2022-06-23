8acb8e37bf6ff4679bb8e1c425f099e014a65841
Active <56242467+doubleactii@users.noreply.github.com>
2022-06-23 15:21:43 -0500

    Separate server and client logic better
    This commit also removes the `pObject` argument inside of `isInCollection`. It was a waste of an argument. Since the type is passed the plugin can infer if it is an object or not. It does not need to be explicitly passed. This frees us the space to use for strictly arguments to be passed into `instance.onDumped`.
    
    Removed circular references from being logged in errors.
    
    Fixed a sanity check from throwing the wrong error.
    
    Removed COLLECTION_LIMIT and instead references it via a variable belonging to the plugin now.
    
    Adds a sanity check before using `VS.Client`
    
    Removed additional checks for typeof()

--- plugin/aRecycle.js
+++ plugin/aRecycle.js
@@ -1,29 +1,24 @@
 (() => {
-	// Client and Server library
 	const engineWaitId = setInterval(() => {
-		if (VS.Client) {
-			clearInterval(engineWaitId);
-			buildRecycle();
-		}
+		clearInterval(engineWaitId);
+		buildRecycle();
 	})
 
 	const buildRecycle = () => {
 		const WARNING_LIMIT = 200;
-		let COLLECTION_LIMIT = 20;
 		const aRecycle = {};
-		aRecycle.version = '0.1.0';
-		// debugging is whether this library is in debug mode. Extra warnings will be thrown in this mode to help explain any issues that may arise.
+		// Debugging is whether this library is in debug mode. Extra warnings will be thrown in this mode to help explain any issues that may arise.
 		aRecycle.debugging = false;
+		aRecycle.collectionLimit = 20;
 		aRecycle.basicCollection = [];
-
-		VS.Client.___EVITCA_aRecycle = true;
-		VS.Client.aRecycle = aRecycle;
+	
+		if (VS.World.getCodeType() !== 'server') VS.Type.setVariables('Client', {___EVITCA_aRecycle: true, aRecycle: aRecycle});
 		VS.global.aRecycle = aRecycle;
 
 		aRecycle.setMaxLimit = function(pLimit) {
 			if (typeof(pLimit) === 'number') {
-				COLLECTION_LIMIT = Math.round(pLimit);
-				if (COLLECTION_LIMIT > WARNING_LIMIT) {
+				this.collectionLimit = Math.round(pLimit);
+				if (this.collectionLimit > WARNING_LIMIT) {
 					console.warn('aRecycle: This is a high value to use for a max limit in a collection! Only use this high of a value if you know what you are doing.')
 				}
 			}
@@ -31,28 +26,33 @@
 
 		aRecycle.collect = function(pCollected, pCollection) {
 			let recycledArray = false;
-			if (pCollected && typeof(pCollected) === 'object' && !Array.isArray((pCollection)) && Object.keys(pCollected).length) {
-				console.error('aRecycle Module [collect]: OOPS! %cpCollected', 'font-weight: bold', ' is a empty object and will NOT be collected.', 'pCollected: ', pCollected);
+			if (pCollected && typeof(pCollected) === 'object' && Object.keys(pCollected).length) {
+				console.error('aRecycle Module [collect]: OOPS! %cpCollected', 'font-weight: bold', ' is an empty object and will NOT be collected.');
+				return;
+			}
+
+			if (!pCollected.type) {
+				console.error('aRecycle Module [collect]: OOPS! %cpCollected', 'font-weight: bold', ' is not a valid object It has no type.');
 				return;
 			}
 
 			if (Array.isArray((pCollection))) {
 				if (pCollected) {
 					if (pCollection.includes(pCollected)) {
-						if (this.debugging) console.error('aRecycle Module [collect]: OOPS! %cpCollected', 'font-weight: bold', 'already belongs to the provided collection.', 'pCollected: ', pCollected);
+						if (this.debugging) console.error('aRecycle Module [collect]: OOPS! %cpCollected', 'font-weight: bold', 'already belongs to the provided collection.');
 						return;
 					}
 					if (Array.isArray((pCollected))) {
 						if (!pCollected.length) {
-							console.error('aRecycle Module [collect]: OOPS! %cpCollected', 'font-weight: bold', 'is an array. But it contains nothing to recycle.', 'pCollected: ', pCollected);
+							console.error('aRecycle Module [collect]: OOPS! %cpCollected', 'font-weight: bold', 'is an array. But it contains nothing to recycle.');
 							return;
 						}
 						recycledArray = true;
 					}
 
 					if (recycledArray) {
-						// if you try to collect a diob to be recycled and the collection you are recyling it to is full, it is deleted instead.
-						if (pCollection.length >= COLLECTION_LIMIT) {
+						// If you try to collect a diob to be recycled and the collection you are recyling it to is full, it is deleted instead.
+						if (pCollection.length >= this.collectionLimit) {
 							for (let i = pCollected.length - 1; i >= 0; i--) {
 								const diob = pCollected[i];
 								if (diob.constructor === Diob) {
@@ -65,9 +65,9 @@
 							}
 							return;
 						}
-						// if this recycledArray has more diobs than the collection can handle, the access is deleted
-						if (pCollected.length + pCollection.length > COLLECTION_LIMIT) {
-							const remainder = pCollected.length - (COLLECTION_LIMIT - pCollection.length);
+						// If this recycledArray has more diobs than the collection can handle, the access is deleted
+						if (pCollected.length + pCollection.length > this.collectionLimit) {
+							const remainder = pCollected.length - (this.collectionLimit - pCollection.length);
 							for (let c = pCollected.length - 1; c >= remainder; c--) {
 								const diob = pCollected[c];
 								if (diob.constructor === Diob) {
@@ -81,17 +81,17 @@
 								}
 							}
 						}
-						// the remaining diobs that are in the recycledArray is now cleaned and processed and added to the collection
+						// The remaining diobs that are in the recycledArray is now cleaned and processed and added to the collection
 						for (let k = pCollected.length - 1; k >= 0; k--) {
 							const diob = pCollected[k];
-							if (diob.onCollected && typeof(diob.onCollected) === 'function') diob.onCollected();
+							if (typeof(diob.onCollected) === 'function') diob.onCollected();
 							if (!pCollection.includes(diob)) pCollection.push(diob);
 							this.cleanDiob(diob);
 						}
 						return;
 					} else {
-						// if you try to collect a diob to be recycled and the collection you are recyling it to is full, it is deleted instead.
-						if (pCollection.length >= COLLECTION_LIMIT) {
+						// If you try to collect a diob to be recycled and the collection you are recyling it to is full, it is deleted instead.
+						if (pCollection.length >= this.collectionLimit) {
 							if (pCollected.constructor === Diob) {
 								this.cleanDiob(pCollected);
 								VS.delDiob(pCollected);
@@ -101,27 +101,29 @@
 							}
 							return;
 						}
-						if (pCollected.onCollected && typeof(pCollected.onCollected) === 'function') pCollected.onCollected();
+						if (typeof(pCollected.onCollected) === 'function') pCollected.onCollected();
 						if (!pCollection.includes(pCollected)) pCollection.push(pCollected);
 						this.cleanDiob(pCollected);
 						return;
 					}
 				} else {
-					console.error('aRecycle Module [collect]: There was nothing passed for the %cpCollected', 'font-weight: bold', 'parameter. Expecting a diob or an object.', 'pCollected: ', pCollected);
+					console.error('aRecycle Module [collect]: There was nothing passed for the %cpCollected', 'font-weight: bold', 'parameter. Expecting a diob or an object.');
 				}
 
 			} else {
-				console.error('aRecycle Module [collect]: Invalid variable type passed for the %cpCollection', 'font-weight: bold', 'parameter. Expecting an array. Collect failed.', 'pCollection: ', pCollection);
+				console.error('aRecycle Module [collect]: Invalid variable type passed for the %cpCollection', 'font-weight: bold', 'parameter. Expecting an array. Collect failed.');
 			}
 		}
 
-		aRecycle.isInCollection = function(pType='Diob', pNum=1, pCollection=[], pObject=false, ...pRest) {
+		aRecycle.isInCollection = function(pType='Diob', pNum=1, pCollection=[], ...pRest) {
 			const reuseArray = [];
 			let added = 0;
 			let quantity = pNum;
+			// Objects do not have a baseType variable
+			let isObject = !VS.Type.getVariable(pType, 'baseType');
 			if (!pCollection.length) {
 				for (let i = 0; i < pNum; i++) {
-					if (pObject) {
+					if (isObject) {
 						reuseArray.push(VS.newObject(pType, ...pRest));
 					} else {
 						reuseArray.push(VS.newDiob(pType, ...pRest));
@@ -134,24 +136,24 @@
 					if (quantity) {
 						let diobInCollection = pCollection[j];
 						if (diobInCollection.type === pType) {
-							// remove it from the collection
+							// Remove it from the collection
 							pCollection.splice(j, 1);
-							// add it to the array that you will be getting from this collection
+							// Add it to the array that you will be getting from this collection
 							reuseArray.push(diobInCollection);
-							// label that this diob is no longer considered to be collection
+							// Label that this diob is no longer considered to be collection
 							diobInCollection.aRecycleCollected = false;
-							// if this diob has a `onDumped` function defined call it.
-							if (diobInCollection.onDumped && typeof(diobInCollection.onDumped) === 'function') diobInCollection.onDumped(...pRest);
+							// If this diob has a `onDumped` function defined call it.
+							if (typeof(diobInCollection.onDumped) === 'function') diobInCollection.onDumped(...pRest);
 							added++;
 							quantity--;
 						}
 					}
 				}
-				// if the amount of diobs we were supposed to get is greater than the diobs we have gotten from the array, we need to generate more.
+				// If the amount of diobs we were supposed to get is greater than the diobs we have gotten from the array, we need to generate more.
 				if (pNum > added) {
 					const missingQuantity = pNum - added;
 					for (let x = 0; x < missingQuantity; x++) {
-						if (pObject) {
+						if (isObject) {
 							reuseArray.push(VS.newObject(pType, ...pRest));
 						} else {
 							reuseArray.push(VS.newDiob(pType, ...pRest));
@@ -184,7 +186,9 @@
 					}
 					if (isInterface) {
 						pDiob.hide();
-						VS.Client.removeInterfaceElement(pDiob.getInterfaceName(), pDiob, true);
+						if (VS.World.getCodeType() !== 'server') {
+							VS.Client.removeInterfaceElement(pDiob.getInterfaceName(), pDiob, true);
+						}
 					}
 					pDiob.playAnimation();
 					pDiob.setTransition();
@@ -205,7 +209,7 @@
 				}
 				pDiob.aRecycleCollected = true;
 				pDiob.inTicker = null;
-				if (pDiob.clean && typeof(pDiob.clean) === 'function') pDiob.clean();
+				if (typeof(pDiob.clean) === 'function') pDiob.clean();
 			}
 		}
 
